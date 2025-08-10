aeec3199676d8ae86353781d574d424d6a88c6f7
DevRoh <paulrohit85@gmail.com>
2025-08-10 21:36:54 +0000

    Advanced Sorting-1 Added

---
+++ Advanced Sorting-1/mergeSort.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void merge(vector<int> &a, vector<int> &b, vector<int> &res)
+{
+    int i = 0; // a
+    int j = 0; // b
+    int k = 0; // res
+    while (i < a.size() && j < b.size())
+    {
+        if (a[i] < b[j])
+        {
+            res[k] = a[i];
+            i++;
+        }
+        else
+        {
+            res[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+    if (i == a.size())
+    {
+        while (j < b.size())
+        {
+            res[k] = b[j];
+            j++;
+            k++;
+        }
+    }
+    else if (j == b.size())
+    {
+        while (i < a.size())
+        {
+            res[k] = a[i];
+            i++;
+            k++;
+        }
+    }
+}
+
+void mergeSort(vector<int>& v) {
+    
+    int n = v.size();
+    if(n==1) return;
+    int n1 = n/2;
+    int n2 = n-n/2;
+    vector<int>a(n1);
+    vector<int>b(n2);
+    //Copy Pasting
+    for(int i=0;i<n1;i++) {
+        a[i] = v[i];
+    }
+    for(int i=0;i<n2;i++) {
+        b[i] = v[i+n1];
+    }
+
+    //Recursion
+    mergeSort(a);
+    mergeSort(b);
+    //merge
+    merge(a,b,v);
+}
+
+int main()
+{
+    int arr[] = {5, 1, 3, 0, 4, 9, 7, 5, 263};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    vector<int> v(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i]<<" ";
+    }
+
+    cout << endl;
+    
+    mergeSort(v);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i]<<" ";
+    }
+}
